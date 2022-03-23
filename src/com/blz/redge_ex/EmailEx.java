826c5fd0c84803ba159dad82e1c810a160ce3160
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-23 14:00:39 +0530

    Added functionality of matching given email with regex pattern

---
+++ src/com/blz/redge_ex/EmailEx.java
@@ -0,0 +1,20 @@
+package com.blz.redge_ex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailEx {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Regex Email pattern program");
+        String email="abc@bridgelabz.co";
+        String regex="^[a-z]+(@[a-z]+.[a-z]{2,3})*$";
+        //creating objects of pattern and matcher.
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        if (matcher.matches())
+            System.out.println("email is valid");
+        else
+            System.out.println("email is invalid");
+    }
+
+}
