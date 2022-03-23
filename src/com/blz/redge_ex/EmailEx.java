85eb6ec5d5ea4533011e7e4d59d3b5fc3f3b4b00
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-23 14:35:36 +0530

    Modified regex pattern to match the optional part in Email

--- src/com/blz/redge_ex/EmailEx.java
+++ src/com/blz/redge_ex/EmailEx.java
@@ -6,15 +6,15 @@ import java.util.regex.Pattern;
 public class EmailEx {
     public static void main(String[] args) {
         System.out.println("Welcome to Regex Email pattern program");
-        String email="abc@bridgelabz.co";
-        String regex="^[a-z]+@[a-z]+.[a-z]{2,3}$";
+        String email="abc.xyz@bridgelabz.co.in";
+        String regex="^[a-z0-9]+[.+_-]([a-z]+)*@[a-z]+.[a-z]{2,3}[._+-][a-z]{2}$";
         //creating objects of pattern and matcher.
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
         if (matcher.matches())
-            System.out.println("email is valid! \nAs mandatory parts abc,@,BridgeLabz,'.',and co matches with regex pattern");
+            System.out.println("email is valid! \nAs optional part xyz matches with regex pattern");
         else
-            System.out.println("email is invalid! \nAs mandatory parts abc,@,BridgeLabz,'.',and co doesn't matches with regex pattern");
+            System.out.println("email is invalid! \nAs optional part xyz doesn't matches with regex pattern");
     }
 
 }
