00b48479bedd8e7d8cb811b9b88b227bfc56188d
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-23 12:57:55 +0530

    Modified the pincode and regex pattern to match the space

--- src/com/blz/redge_ex/PinCodeEx.java
+++ src/com/blz/redge_ex/PinCodeEx.java
@@ -7,14 +7,14 @@ public class PinCodeEx {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Regex pinCode pattern program");
-        String pinCode = "400088B";
-        String regex = "^[1-9]{1}[0-9]{5}$";
+        String pinCode = "400 088";
+        String regex = "^[1-9]{1}[0-9]{2}\\s?[0-9]{3}$";
         //creating pattern and matcher objects
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(pinCode);
         if(matcher.matches())
-        System.out.println("PinCode is Valid");
+        System.out.println("PinCode is Valid! as space in pinCode matches regex pattern");
         else
-            System.out.println("PinCode is Invalid as there is alphabet at End");
+            System.out.println("PinCode is Invalid! as space in pinCode does not match the regex pattern");
     }
 }
