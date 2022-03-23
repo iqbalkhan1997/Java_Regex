02f7d3e8fb51f2f564ec877c9d6fa2c0b56ef99d
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-23 14:12:32 +0530

    Modified the exiting regex pattern to match the 2nd mandatory part in Email

--- src/com/blz/redge_ex/EmailEx.java
+++ src/com/blz/redge_ex/EmailEx.java
@@ -7,14 +7,14 @@ public class EmailEx {
     public static void main(String[] args) {
         System.out.println("Welcome to Regex Email pattern program");
         String email="abc@bridgelabz.co";
-        String regex="^[a-z]+(@[a-z]+.[a-z]{2,3})*$";
+        String regex="^[a-z]+@[a-z]+(.[a-z]{2,3})*$";
         //creating objects of pattern and matcher.
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
         if (matcher.matches())
-            System.out.println("email is valid");
+            System.out.println("email is valid! As abc,@ and BridgeLabz mandatory parts matches with regex pattern");
         else
-            System.out.println("email is invalid");
+            System.out.println("email is invalid! As abc,@ and BridgeLabz mandatory parts doesn't matches with regex pattern");
     }
 
 }
