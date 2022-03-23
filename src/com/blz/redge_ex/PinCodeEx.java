8c1c8e551ac7b0c4fcb5f83c4c17f4a03c30d678
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-23 12:11:02 +0530

    Added functionality to check pincode and regex pattern matches or not

--- src/com/blz/redge_ex/PinCodeEx.java
+++ src/com/blz/redge_ex/PinCodeEx.java
@@ -1,8 +1,20 @@
 package com.blz.redge_ex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PinCodeEx {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Regex pinCode pattern program");
+        String pinCode = "400088";
+        String regex = "^[0-9]{6}$";
+        //creating pattern and matcher objects
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(pinCode);
+        if(matcher.matches())
+        System.out.println("PinCode is Valid");
+        else
+            System.out.println("PinCode is Invalid");
     }
 }
