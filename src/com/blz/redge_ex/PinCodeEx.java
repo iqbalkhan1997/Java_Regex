e872cfec30b135d1882ef4afcbc16e36fc897f86
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-23 11:49:03 +0530

    Added welcome message

---
+++ src/com/blz/redge_ex/PinCodeEx.java
@@ -0,0 +1,8 @@
+package com.blz.redge_ex;
+
+public class PinCodeEx {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Regex pinCode pattern program");
+    }
+}
