d88f4f917c8b8a930ca99f61c1072ce8149c6ca7
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-23 14:45:23 +0530

    Optimized the regex pattern code  by supporting optional parts

--- src/com/blz/redge_ex/EmailEx.java
+++ src/com/blz/redge_ex/EmailEx.java
@@ -7,14 +7,13 @@ public class EmailEx {
     public static void main(String[] args) {
         System.out.println("Welcome to Regex Email pattern program");
         String email="abc.xyz@bridgelabz.co.in";
-        String regex="^[a-z0-9]+[.+_-]([a-z]+)*@[a-z]+.[a-z]{2,3}[._+-][a-z]{2}$";
+        String regex="^[a-z0-9]+([.+_-][a-z]+)*@[a-z]+.[a-z]{2,3}(.[a-z]{2})*$";
         //creating objects of pattern and matcher.
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
         if (matcher.matches())
-            System.out.println("email is valid! \nAs optional part xyz matches with regex pattern");
+            System.out.println("email is valid! \nAs optional part xyz and country code matches with regex pattern");
         else
-            System.out.println("email is invalid! \nAs optional part xyz doesn't matches with regex pattern");
+            System.out.println("email is invalid! \nAs optional part xyz and country code doesn't matches with regex pattern");
     }
-
 }
