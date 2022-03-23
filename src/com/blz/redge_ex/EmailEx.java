47751e208ddd3cdea5fb8de1f15352533ee07704
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-23 14:21:33 +0530

    Refactored regex code to match given email for all mandatory parts

--- src/com/blz/redge_ex/EmailEx.java
+++ src/com/blz/redge_ex/EmailEx.java
@@ -7,14 +7,14 @@ public class EmailEx {
     public static void main(String[] args) {
         System.out.println("Welcome to Regex Email pattern program");
         String email="abc@bridgelabz.co";
-        String regex="^[a-z]+@[a-z]+(.[a-z]{2,3})*$";
+        String regex="^[a-z]+@[a-z]+.[a-z]{2,3}$";
         //creating objects of pattern and matcher.
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
         if (matcher.matches())
-            System.out.println("email is valid! As abc,@ and BridgeLabz mandatory parts matches with regex pattern");
+            System.out.println("email is valid! \nAs mandatory parts abc,@,BridgeLabz,'.',and co matches with regex pattern");
         else
-            System.out.println("email is invalid! As abc,@ and BridgeLabz mandatory parts doesn't matches with regex pattern");
+            System.out.println("email is invalid! \nAs mandatory parts abc,@,BridgeLabz,'.',and co doesn't matches with regex pattern");
     }
 
 }
